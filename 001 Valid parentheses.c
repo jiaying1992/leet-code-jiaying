91e992a2ce63d5fa65bfa99204a2451fa42baa51
jiaying1992 <1439352516@qq.com>
2017-04-26 10:36:20 +0800

    Brackets pairs

--- 001 Valid parentheses.c
+++ 001 Valid parentheses.c
@@ -1,8 +1,7 @@
-Valid parentheses
-=========================================================================================================================
+# Description
 Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
--------------------------------------------------------------------------------------------------------------------------
+# Solution
 bool isValid(char* s) {
     int len=strlen(s);
     int num=0;
