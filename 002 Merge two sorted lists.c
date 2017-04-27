0f2bb5b99292380a1d590d8a0c37b5aaeb20d55d
jiaying1992 <1439352516@qq.com>
2017-04-27 15:24:17 +0800

    Sorted list

--- 002 Merge two sorted lists.c
+++ 002 Merge two sorted lists.c
@@ -1,6 +1,8 @@
 # Description
+====================================================================================
 Merge two sorted linked lists and return it as a new list. 
 The new list should be made by splicing together the nodes of the first two lists.
+------------------------------------------------------------------------------------
 # Solution:
 /**
  * Definition for singly-linked list.
