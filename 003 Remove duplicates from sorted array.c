a43b5481a7ae3bd43ac1afe4f3502c08bcd94ed9
jiaying1992 <1439352516@qq.com>
2017-04-27 15:26:16 +0800

    Sorted array

--- 003 Remove duplicates from sorted array.c
+++ 003 Remove duplicates from sorted array.c
@@ -1,10 +1,12 @@
-# Descriptions
+# Description
+=========================================================================================================================
 Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 Do not allocate extra space for another array, you must do this in place with constant memory.
 For example,
 Given input array nums = [1,1,2],
 Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. 
 It doesn't matter what you leave beyond the new length.
+--------------------------------------------------------------------------------------------------------------------------
 # Solution
 int removeDuplicates(int* nums, int numsSize) {
     if(numsSize<2)
@@ -23,6 +25,5 @@ int removeDuplicates(int* nums, int numsSize) {
             nums[i-num]=nums[i];
         }
     }
-    return numsSize-num;
-    
+    return numsSize-num;   
 }
