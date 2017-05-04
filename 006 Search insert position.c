39568dd77cf5f0e25397f88bcbe002d09134de60
jiaying <1439352516@qq.com>
2017-05-04 15:31:28 +0800

    Sorted array

--- 006 Search insert position.c
+++ 006 Search insert position.c
@@ -1,4 +1,4 @@
-# Description
+﻿# Description
 ==================================================================================
 Given a sorted array and a target value, return the index if the target is found. 
 If not, return the index where it would be if it were inserted in order.
@@ -9,6 +9,7 @@ Here are few examples.
 [1,3,5,6], 7 → 4
 [1,3,5,6], 0 → 0
 -----------------------------------------------------------------------------------
+# Solution
 int searchInsert(int* nums, int numsSize, int target) {
     int i;
     for(i=0; i<numsSize; i++)
