442386f08437ba35eae10dbdba18cc515e644be9
jiaying <1439352516@qq.com>
2017-05-24 20:55:19 +0800

    Tree

---
+++ 017 Symmetric tree.cpp
@@ -0,0 +1,79 @@
+# Description
+=================================================================================================
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+---------------------------------------------------------------------------------------------------
+# Solution
+1°¢µ›πÈ
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        return help(root->left, root->right);
+    }
+    bool help(TreeNode* left, TreeNode* right)
+    {
+        if(!left && !right)
+        {
+            return true;
+        }
+        if(!left || !right)
+        {
+            return false;
+        }
+        return help(left->left, right->right) && help(left->right, right->left) && (right->val==left->val);
+    }
+};
+2°¢∂”¡–
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        TreeNode *left, *right;
+        if (!root)
+            return true;
+        
+        queue<TreeNode*> q1, q2;
+        q1.push(root->left);
+        q2.push(root->right);
+        while (!q1.empty() && !q2.empty()){
+            left = q1.front();
+            q1.pop();
+            right = q2.front();
+            q2.pop();
+            if (NULL == left && NULL == right)
+                continue;
+            if (NULL == left || NULL == right)
+                return false;
+            if (left->val != right->val)
+                return false;
+            q1.push(left->left);
+            q1.push(left->right);
+            q2.push(right->right);
+            q2.push(right->left);
+        }
+        return true;
+    }
+};
