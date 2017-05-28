b00848565e3f40949ab8bd775b6ff2a74b0dfb1c
jiaying <1439352516@qq.com>
2017-05-28 15:40:57 +0800

    binary search tree

---
+++ 020 Convert sorted array to binary search tree.cpp
@@ -0,0 +1,27 @@
+# Description
+==================================================================================================
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+--------------------------------------------------------------------------------------------------
+# Solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if(nums.size()==0) return NULL;
+        int middle=nums.size()/2;
+        TreeNode *root=new TreeNode(nums[middle]);
+        vector<int> left(nums.begin(), nums.begin()+middle);
+        vector<int> right(nums.begin()+middle+1, nums.end());
+        root->left=sortedArrayToBST(left);
+        root->right=sortedArrayToBST(right);
+        return root;
+    }
+};
