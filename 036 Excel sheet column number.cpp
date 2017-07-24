7173e254f10ba484aca0283fff15b23b57b006ef
jiaying <1439352516@qq.com>
2017-07-24 10:09:59 +0800

    string to int

---
+++ 036 Excel sheet column number.cpp
@@ -0,0 +1,25 @@
+# Description
+============================================================================================
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+---------------------------------------------------------------------------------------------
+# Solution
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int result=0;
+        for(int i=0; i<s.size(); i++)
+        {
+            result=result*26+(s[i]-'A'+1);
+        }
+        return result;
+    }
+};
